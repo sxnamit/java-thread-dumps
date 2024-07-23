73c91979e6c5111af3d8202b52bde2262b5daae7
Amit Saxena <sxnamit.git@gmail.com>
2024-07-23 14:17:41 +0530

    Zeroth commit

---
+++ src/main/java/com/sxnamit/jtd/App.java
@@ -0,0 +1,13 @@
+package com.sxnamit.jtd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
